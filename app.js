6191aebb7084973b639895f135f065d4c12278c7
David Lee <david@david-2.local>
2015-04-18 00:00:45 -0700

    authentication so far

--- app.js
+++ app.js
@@ -2,6 +2,7 @@
 var express = require('express');
 var passport = require('passport');
 var InstagramStrategy = require('passport-instagram').Strategy;
+var FacebookStrategy = require('passport-facebook').Strategy;
 var http = require('http');
 var path = require('path');
 var handlebars = require('express-handlebars');
@@ -10,6 +11,7 @@ var session = require('express-session');
 var cookieParser = require('cookie-parser');
 var dotenv = require('dotenv');
 var Instagram = require('instagram-node-lib');
+var graph = require('fbgraph');
 var mongoose = require('mongoose');
 var app = express();
 
@@ -24,6 +26,12 @@ var INSTAGRAM_CALLBACK_URL = process.env.INSTAGRAM_CALLBACK_URL;
 var INSTAGRAM_ACCESS_TOKEN = "";
 Instagram.set('client_id', INSTAGRAM_CLIENT_ID);
 Instagram.set('client_secret', INSTAGRAM_CLIENT_SECRET);
+
+var FACEBOOK_APP_ID = process.env.FACEBOOK_APP_ID;
+var FACEBOOK_APP_SECRET = process.env.FACEBOOK_APP_SECRET;
+var FACEBOOK_CALLBACK_URL = process.env.FACEBOOK_CALLBACK_URL;
+graph.setAppSecret(FACEBOOK_APP_SECRET);
+
 
 //connect to database
 mongoose.connect(process.env.MONGODB_CONNECTION_URL);
@@ -63,11 +71,48 @@ passport.use(new InstagramStrategy({
     models.User.findOrCreate({
       "name": profile.username,
       "id": profile.id,
-      "access_token": accessToken 
+      "access_token": accessToken
     }, function(err, user, created) {
       
       // created will be true here
       models.User.findOrCreate({}, function(err, user, created) {
+	//  var user = new models.User();
+        //  user.ig_access_token = accessToken;
+        //  user.save();
+        // created will be false here
+        process.nextTick(function () {
+          // To keep the example simple, the user's Instagram profile is returned to
+          // represent the logged-in user.  In a typical application, you would want
+          // to associate the Instagram account with a user record in your database,
+          // and return that user instead.
+          return done(null, user);
+        });
+      })
+    });
+  }
+));
+
+// Use the FacebookStrategy within Passport.
+//   Strategies in Passport require a `verify` function, which accept
+//   credentials (in this case, an accessToken, refreshToken, and Facebook
+//   profile), and invoke a callback with a user object.
+passport.use(new FacebookStrategy({
+    clientID: FACEBOOK_APP_ID,
+    clientSecret: FACEBOOK_APP_SECRET,
+    callbackURL: FACEBOOK_CALLBACK_URL
+  },
+  function(accessToken, refreshToken, profile, done) {
+    // asynchronous verification, for effect...
+    models.User.findOrCreate({
+      "name": profile.username,
+      "id": profile.id,
+      "access_token": accessToken,
+    }, function(err, user, created) {
+      
+      // created will be true here
+      models.User.findOrCreate({}, function(err, user, created) {
+       // user.fb_access_token = accessToken;
+       // user.save();
         // created will be false here
         process.nextTick(function () {
           // To keep the example simple, the user's Instagram profile is returned to
@@ -80,6 +125,7 @@ passport.use(new InstagramStrategy({
     });
   }
 ));
+
 
 //Configures the Template engine
 app.engine('handlebars', handlebars({defaultLayout: 'layout'}));
@@ -107,8 +153,27 @@ function ensureAuthenticated(req, res, next) {
   if (req.isAuthenticated()) { 
     return next(); 
   }
+  req.flash('error', 'You must be logged in to do that.')
   res.redirect('/login');
 }
+
+/*
+function ensureAuthenticatedInstagram(req, res, next) {
+  if (req.isAuthenticated() || req.user.igID) {
+    return next();
+  }
+  req.flash('error', 'You must be logged in to do that.')
+  res.redirect('/login');
+}
+
+function ensureAuthenticatedFacebook(req, res, next) {
+  if (req.isAuthenticated() || req.user.fbID) {
+    return next();
+  }
+  req.flash('error', 'You must be logged in to do that.')
+  res.redirect('/login');
+}
+*/
 
 //routes
 app.get('/', function(req, res){
@@ -118,8 +183,63 @@ app.get('/', function(req, res){
 app.get('/login', function(req, res){
   res.render('login', { user: req.user });
 });
+/*
+app.get('/account', ensureAuthenticated, function(req, res){
+  if (req.user.provider == 'facebook')
+  {
+    var query  = models.User.where({ name: req.user.username });
+    query.findOne(function (err, user) {
+      if (err) return handleError(err);
+      if (user) {
+        graph.get('/' + user.id + '?fields=picture',
+                  { access_token: user.access_token },
+                  function(err2, res2) {
+                    res.render('account', {user: req.user,
+                         providerFacebook: req.user.provider == 'facebook',
+                         providerInstagram: req.user.provider == 'instagram',
+                         fbpic: res2.picture.data.url});
+                  });
+        
+      }
+    });
+  }
+  else
+  {
+    res.render('account', {user: req.user,
+                           providerFacebook: req.user.provider == 'facebook',
+                           providerInstagram: req.user.provider == 'instagram'});
+  }
+  
+});*/
+
+
+/*app.get('/likes', ensureAuthenticated, function(req, res){
+  var query = models.User.where({ name: req.user.username });
+  query.findOne(function (err, user) {
+    if (err) return handleError(err);
+    if (user) {
+      graph.get('/' + user.id + "?fields=likes", 
+                { access_token: user.access_token },
+*/
+
+//app.get('/fbfeed', ensureAuthenticated, function(req, res){
+
+//  var query =models.User.where({ name: req.user.username });
+  
+  
 
 app.get('/account', ensureAuthenticated, function(req, res){
+  
+  var query = models.User.where({ name: req.user.username });
+  
+  graph.setAccessToken(query.access_token);
+  console.log(req.user.access_token);
+  graph.get('/me', function(err, res){
+    console.log(res);
+  });
+  
+  console.log(req.user);
+
   res.render('account', {user: req.user});
 });
 
@@ -146,9 +266,25 @@ app.get('/photos', ensureAuthenticated, function(req, res){
           res.render('photos', {photos: imageArr});
         }
       }); 
+    
+      /*Facebook.res.id.user.home({
+        access_token: user.access_token,
+        complete: function(data){
+          var imageArr = data.map(function(item){
+            tempJSON = {};
+            tempJSON.url = post.picture;
+            tempJSON.caption = post.caption;
+          });
+        }
+      });*/
     }
   });
 });
+
+
+
+
+
 
 
 // GET /auth/instagram
@@ -163,6 +299,7 @@ app.get('/auth/instagram',
     // function will not be called.
   });
 
+
 // GET /auth/instagram/callback
 //   Use passport.authenticate() as route middleware to authenticate the
 //   request.  If authentication fails, the user will be redirected back to the
@@ -173,6 +310,32 @@ app.get('/auth/instagram/callback',
   function(req, res) {
     res.redirect('/photos');
   });
+
+
+// GET /auth/facebook
+//   Use passport.authenticate() as route middleware to authenticate the
+//   request.  The first step in Facebook authentication will involve
+//   redirecting the user to facebook.com.  After authorization, Facebook
+//   will redirect the user back to this application at /auth/facebook/callback
+app.get('/auth/facebook',
+  passport.authenticate('facebook', { scope: ['user_posts', 'user_likes'] }),
+  function(req, res){
+    // The request will be redirected to Facebook for authentication, so this
+    // function will not be called.
+  });
+
+
+// GET /auth/facebook/callback
+//   Use passport.authenticate() as route middleware to authenticate the
+//   request.  If authentication fails, the user will be redirected back to the
+//   login page.  Otherwise, the primary route function function will be called,
+//   which, in this example, will redirect the user to account info.
+app.get('/auth/facebook/callback', 
+  passport.authenticate('facebook', { failureRedirect: '/login'}),
+  function(req, res) {
+    res.redirect('/account');
+  });
+
 
 app.get('/logout', function(req, res){
   req.logout();
--- models.js
+++ models.js
@@ -5,7 +5,11 @@ var findOrCreate = require('mongoose-findorcreate');
 var userSchema = mongoose.Schema({
 	"name" : { type: String },
 	"id" : { type: String },
-	"access_token" : { type: String }
+	"access_token" : { type: String },
+//	"igID" : { type: String },
+//	"fbID" : { type: String },
+//	"ig_access_token" : { type: String },
+//	"fb_access_token" : { type: String }
 });
 
 userSchema.plugin(findOrCreate);
